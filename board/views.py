537b929b556000c7f1a39117417e681cb9c7dcee
문승기 <moon44432@naver.com>
2020-08-29 00:37:58 +0900

    implement signin/signout

--- board/views.py
+++ board/views.py
@@ -10,16 +10,65 @@ from .forms import *
 from .models import *
 
 def index(req):
-    pass
+    return render(req, 'index.html')
 
 def sign_up(req):
-    pass
+    if req.user.is_authenticated:
+        return HttpResponse(status = 404)
+    if req.method == 'GET':
+        return sign_up_form(req)
+    if req.method == 'POST':
+        return sign_up_post(req)
+    return HttpResponse(status = 404)
+
+def sign_up_form(req):
+    return render(req, 'auth/sign_up.html', {'form': SignUpForm(),})
+
+def sign_up_post(req):
+    form = SignUpForm(req.POST)
+    if not form.is_valid():
+        return HttpResponse(status = 400)
+
+    user = User.objects.create_user(
+        username = form.cleaned_data['username'],
+        password = form.cleaned_data['password'],
+        email = form.cleaned_data['email'],
+    )
+    user.save()
+
+    return redirect('sign_in')
 
 def sign_in(req):
-    pass
+    if req.user.is_authenticated:
+        return HttpResponse(status = 404)
+    if req.method == 'GET':
+        return sign_in_form(req)
+    if req.method == 'POST':
+        return sign_in_post(req)
+    return HttpResponse(status = 404)
+
+def sign_in_form(req):
+    return render(req, 'auth/sign_in.html', {'form': SignInForm(),})
+
+def sign_in_post(req):
+    form = SignInForm(req.POST)
+    if not form.is_valid():
+        return HttpResponse(status = 400)
+
+    user = authenticate(
+        username = form.cleaned_data['username'],
+        password = form.cleaned_data['password'],
+    )
+
+    if user:
+        login(req, user)
+        return redirect('index')
+    else:
+        return HttpResponse(status = 401)
 
 def sign_out(req):
-    pass
+    logout(req)
+    return redirect('index')
 
 def category_view_head(req):
     pass
