1e01ecbbd6811e10bfd586c08fcaa0a2129ebf7d
문승기 <moon44432@naver.com>
2020-08-29 00:06:27 +0900

    migrate database

--- board/models.py
+++ board/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Your models here
+class Category(models.Model):
+    name = models.CharField(max_length = 10)
+    creator = models.ForeignKey(User, on_delete = models.CASCADE)
+    is_deleted = models.BooleanField(default = False)
+
+class Article(models.Model):
+    title = models.CharField(max_length = 40)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    last_updated_at = models.DateTimeField(auto_now = True)
+    is_deleted = models.BooleanField(default = False)
+    like = models.ManyToManyField(User, related_name = 'like_user')
+
+class Comment(models.Model):
+    comment = models.CharField(max_length = 300)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    article = models.ForeignKey(Article, on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
+    last_updated_at = models.DateTimeField(auto_now = True)
+    is_deleted = models.BooleanField(default = False)
--- board/views.py
+++ board/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+import math
 
-# Your code goes here
+from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+
+from .forms import *
+from .models import *
+
+def index(req):
+    pass
+
+def sign_up(req):
+    pass
+
+def sign_in(req):
+    pass
+
+def sign_out(req):
+    pass
+
+def category_view_head(req):
+    pass
+
+def category_view(req):
+    pass
+
+def create_category(req):
+    pass
+
+def delete_category(req):
+    pass
+
+def get_article(req):
+    pass
+
+def compose_article(req):
+    pass
+
+def edit_article(req):
+    pass
+
+def delete_article(req):
+    pass
+
+def compose_comment(req):
+    pass
+
+def delete_comment(req):
+    pass
+
+def like(req):
+    pass
+
+def profile_index(req):
+    pass
+
+def profile_articles(req):
+    pass
